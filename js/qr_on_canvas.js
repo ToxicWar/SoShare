3a6e3be556508e263cf6d1a151c976893dced172
3bl3gamer <3bl3gamer@gmail.com>
2014-03-29 08:48:38 +0000

    QR drawer on canvas

---
+++ js/qr_on_canvas.js
@@ -0,0 +1,24 @@
+function drawOnCanvasAsQR(canvas, text) {
+	var rc = canvas.rc || canvas.getContext("2d");
+	var s = window.devicePixelRatio || 1;
+	var w = canvas.offsetWidth * s;
+	var h = canvas.offsetHeight * s;
+	
+	if (canvas.width != w) canvas.width = w;
+	if (canvas.height != h) canvas.height = h;
+	
+	var qr = qrcode(1, 'L');
+	qr.addData(text);
+	qr.make();
+	var c = qr.getModuleCount();
+	
+	rc.save();
+	rc.clearRect(0, 0, w, h);
+	rc.scale(w/c*s, h/c*s);
+	for (var i=0; i<c; i++) {
+		for (var j=0; j<c; j++) {
+			if (qr.isDark(j,i)) rc.fillRect(i, j, 1, 1);
+		}
+	}
+	rc.restore();
+}

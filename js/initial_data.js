92c377e5e26759e9c47e1fd07200bea96c01e9f9
Anton Larkin <toxicwar94@yandex.ru>
2014-03-30 13:38:01 +0400

    Changed initial data Updated camera

--- js/initial_data.js
+++ js/initial_data.js
@@ -1,4 +1,4 @@
-// QR recognition
+// Initial data
 var durov = {'url': 'http://vk.com/id1', 'title':'Павел Дуров', 'icon':'/images/icons/vk.png'};
 var tardy = {'url': 'https://twitter.com/kirillirik', 'title': 'Tardy Lightfoot', 'icon': '/images/icons/twitter.png'}
-Storage.setVariable('recognitionQR', [durov, tardy]);
+Storage.setVariable('initial_data', [durov, tardy]);
